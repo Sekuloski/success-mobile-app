881256ca72c9e084c94486796a1f9d8bda5acab2
Sekuloski <sekuloski.bojan18@gmail.com>
2023-06-06 18:26:32 +0200

    fix: Payments field added to request

--- app/src/main/java/mk/sekuloski/success/data/remote/dto/PaymentRequest.kt
+++ app/src/main/java/mk/sekuloski/success/data/remote/dto/PaymentRequest.kt
@@ -1,17 +1,7 @@
 package mk.sekuloski.success.data.remote.dto
 
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.Serializer
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.json.JsonObject
-import mk.sekuloski.success.ExpenseType
-import mk.sekuloski.success.PaymentType
-import org.json.JSONObject
-import java.time.LocalDateTime
 import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 
 @Serializable
 data class PaymentRequest (
@@ -23,6 +13,7 @@ data class PaymentRequest (
     val expense_type: Int,
     val cash: Boolean,
     val monthly: Boolean,
+    val payments: Int,
     val credit: Boolean,
     val interest: Double,
     val location: Int,
