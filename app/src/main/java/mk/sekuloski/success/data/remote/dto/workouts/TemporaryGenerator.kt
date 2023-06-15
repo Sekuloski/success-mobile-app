9c69e1cd981ce89bcdd5fd40e2dba2cb7385dcb9
Sekuloski <sekuloski.bojan18@gmail.com>
2023-06-15 15:25:43 +0200

    feat: Initial Workout fragment, with rep count and exercise changing

--- app/src/main/java/mk/sekuloski/success/data/remote/dto/workouts/TemporaryGenerator.kt
+++ app/src/main/java/mk/sekuloski/success/data/remote/dto/workouts/TemporaryGenerator.kt
@@ -3,9 +3,10 @@ package mk.sekuloski.success.data.remote.dto.workouts
 fun getWorkouts(): List<Workout>
 {
     val ringPullUps = Exercise("Ring Pull Ups", 5, 0, listOf(10, 9, 9, 8, 7), rest_time = 180)
-    val weightedPullUps = Exercise("10kg Pull Ups", 5, 10, listOf(2, 1, 1, 1, 1), rest_time = 180, previous_progression = ringPullUps)
+    val weightedPullUps = Exercise("20kg Pull Ups", 5, 20, listOf(2, 1, 1, 1, 1), rest_time = 180, previous_progression = ringPullUps)
     val ringPushUps = Exercise("Ring Push Ups", 5, 0, listOf(20, 18, 17, 16, 15), rest_time = 180)
-    val weightedPushUps = Exercise("10kg Push Ups", 5, 10, listOf(10, 8, 7), rest_time = 180, previous_progression = ringPushUps)
+    val weightedPushUps = Exercise("20kg Push Ups", 5, 20, listOf(10, 8, 7), rest_time = 180, previous_progression = ringPushUps)
+    val weightedDips = Exercise("20kg Dips", 5, 20, listOf(10, 8, 7), rest_time = 180)
     val dragonFlagRaises = Exercise("Dragon Flags", 5, 0, listOf(10, 8, 7), rest_time = 180)
     val lSitHang = Exercise("L Sit Hang", 5, 0, listOf(10, 8, 7), rest_time = 180)
     val squats = Exercise("Squats", 5, 20, listOf(10, 8, 7), rest_time = 180)
@@ -13,10 +14,11 @@ fun getWorkouts(): List<Workout>
     ringPullUps.next_progression = weightedPullUps
     ringPushUps.next_progression = weightedPushUps
 
+    val workout0 = Workout("Weighted Full Body", listOf(weightedPullUps, weightedDips, lSitHang, squats), listOf(0, 2, 4))
     val workout1 = Workout("Weighted Full Body", listOf(weightedPullUps, weightedPushUps, dragonFlagRaises, squats), listOf(0, 2, 4))
     val workout2 = Workout("Body Weight on Rings", listOf(ringPullUps, ringPushUps, lSitHang, pistolSquats), listOf(1, 3, 5))
     val workout3 = Workout("Full Body 3", listOf(ringPullUps, weightedPullUps, ringPushUps, weightedPushUps), listOf(0, 2, 4))
     val workout4 = Workout("Full Body 4", listOf(ringPullUps, weightedPullUps, ringPushUps, weightedPushUps), listOf(1, 3, 5))
 
-    return listOf(workout1, workout2, workout3, workout4)
+    return listOf(workout0, workout1, workout2, workout3, workout4)
 }
--- app/src/main/java/mk/sekuloski/success/fragments/workouts/WorkoutFragment.kt
+++ app/src/main/java/mk/sekuloski/success/fragments/workouts/WorkoutFragment.kt
@@ -1,6 +1,9 @@
 package mk.sekuloski.success.fragments.workouts
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +13,8 @@ import kotlinx.coroutines.MainScope
 import mk.sekuloski.success.R
 import mk.sekuloski.success.data.remote.dto.workouts.Workout
 import mk.sekuloski.success.databinding.FragmentWorkoutBinding
+import java.io.InputStream
+import java.net.URL
 
 
 class WorkoutFragment(_workout: Workout) : Fragment(R.layout.fragment_workouts), CoroutineScope by MainScope() {
@@ -28,10 +33,77 @@ class WorkoutFragment(_workout: Workout) : Fragment(R.layout.fragment_workouts),
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnAdd.setOnClickListener {
+            val value = binding.etReps.text.toString().toInt() + 1
+            binding.etReps.text = value.toString()
+        }
+        binding.btnRemove.setOnClickListener {
+            if (binding.etReps.text.toString().toInt() != 0)
+            {
+                val value = binding.etReps.text.toString().toInt() - 1
+                binding.etReps.text = value.toString()
+            }
+        }
+
+        startWorkout()
     }
 
     override fun onResume() {
         super.onResume()
+    }
+
+    fun startWorkout()
+    {
+        val timer = object: CountDownTimer(20000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                if (millisUntilFinished % 1000 == 0L)
+                {
+                    Log.v("Workout", "Timer: $millisUntilFinished")
+                }
+            }
+
+            override fun onFinish() {
+                Log.v("Workout", "Finished!")
+            }
+        }
+        var currentExercise = 0
+        var currentSet = 0
+        val currentReps = mutableListOf(0, 0, 0, 0, 0)
+
+        try {
+            val inputStream = URL("https://upload.wikimedia.org/wikipedia/commons/5/59/Dipexercise.svg").content as InputStream
+
+            binding.ivExercise.setImageDrawable(Drawable.createFromStream(inputStream, "Dips"))
+        } catch (e: Exception) {
+            Log.e("Workout", "Can't download image!")
+        }
+
+        binding.tvExerciseName.text = workout.exercises[0].name
+        binding.tvReps.text = currentReps.toString()
+
+        binding.btnNext.setOnClickListener {
+            if (currentSet != workout.exercises[currentExercise].sets - 1) {
+                currentReps[currentSet] = binding.etReps.text.toString().toInt()
+                binding.tvReps.text = currentReps.toString()
+                currentSet++
+            }
+            else {
+                if (currentExercise != workout.exercises.size - 1) {
+                    currentReps[currentSet] = binding.etReps.text.toString().toInt()
+                    binding.tvReps.text = currentReps.toString()
+
+                    currentExercise++
+                    currentSet = 0
+                    binding.tvExerciseName.text = workout.exercises[currentExercise].name
+                }
+                else
+                {
+                    Log.v("Workout", "Workout Finished!")
+                }
+            }
+//            timer.start()
+        }
     }
 
     override fun onDestroyView() {
