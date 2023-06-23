655ed288a4d4ddc1fd16bb1c292834ba4c8cce38
Sekuloski <sekuloski.bojan18@gmail.com>
2023-06-23 23:15:39 +0200

    feat: Workout Card on Home Fragment

--- app/src/main/java/mk/sekuloski/success/fragments/HomeFragment.kt
+++ app/src/main/java/mk/sekuloski/success/fragments/HomeFragment.kt
@@ -5,13 +5,24 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
@@ -21,15 +32,18 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.highlight.Highlight
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
-import mk.sekuloski.success.MainActivity
 import mk.sekuloski.success.R
 import mk.sekuloski.success.data.remote.dto.finances.FinancesMain
+import mk.sekuloski.success.data.remote.dto.workouts.Exercise
+import mk.sekuloski.success.data.remote.dto.workouts.Workout
 import mk.sekuloski.success.data.remote.services.finances.FinancesService
 import mk.sekuloski.success.data.remote.services.workouts.WorkoutsService
 import mk.sekuloski.success.databinding.FragmentHomeBinding
 
-class HomeFragment(private val financesService: FinancesService, private val workoutService: WorkoutsService) : Fragment(R.layout.fragment_home), CoroutineScope by MainScope(),
+class HomeFragment(
+    private val financesService: FinancesService,
+    private val workoutService: WorkoutsService
+) : Fragment(R.layout.fragment_home), CoroutineScope by MainScope(),
     com.github.mikephil.charting.listener.OnChartValueSelectedListener {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
@@ -41,96 +55,208 @@ class HomeFragment(private val financesService: FinancesService, private val wor
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
             .apply {
                 composeView.setContent {
-                    Home()
+                    Home(Modifier.fillMaxSize())
                 }
             }
         return binding.root
     }
 
     @Composable
-    fun Home() {
-        var financesMain by remember {
-            mutableStateOf(FinancesMain(0, 0, 0, 0, 0, 0))
-        }
-        LaunchedEffect (key1 = true) {
-            financesMain = financesService.getMainInfo()
-        }
-        Column {
-            TextRow("Amount Left", financesMain.amount_left)
-            TextRow("Bank Amount", financesMain.bank)
-            TextRow("Cash Amount", financesMain.cash)
-            TextRow("Expenses", financesMain.expenses)
+    fun Home(modifier: Modifier) {
+        Column(
+            modifier = modifier
+        ) {
+            FinancesHome(modifier.weight(1f))
+            WorkoutHome(modifier.weight(1f))
         }
     }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-////        binding.swipeRefresh.setOnRefreshListener {
-////            update()
-////        }
-//
-////        launch {
-////            val newData = financesService.getMainInfo()
-////            if (newData != null) {
-////                binding.tvAmountLeft.text = newData.amount_left.toString()
-////                binding.tvBank.text = newData.bank.toString()
-////                binding.tvCash.text = newData.cash.toString()
-////                binding.tvExpenses.text = newData.expenses.toString()
-////                binding.swipeRefresh.isRefreshing = false
-////            }
-////            binding.tvWorkoutStatus.text = workoutService.getWorkoutStatus().toString()
-////        }
-//    }
+    @Composable
+    private fun FinancesHome(modifier: Modifier = Modifier) {
+        var financesMain by remember {
+            mutableStateOf(FinancesMain(0, 0, 0, 0, 0, 0))
+        }
+        LaunchedEffect(key1 = true) {
+            financesMain = financesService.getMainInfo()
+        }
+        Column(
+            modifier = modifier
+        ) {
+            Text(
+                "Financial Situation:",
+                modifier = modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .padding(start = 48.dp, top = 36.dp),
+                fontSize = 28.sp,
+                color = colorResource(R.color.md_theme_dark_onBackground),
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(3f),
+                contentAlignment = Alignment.Center
+            )
+            {
+                FinancesCard(Modifier
+                    .fillMaxHeight()
+                    .align(Alignment.Center), financesMain)
+            }
+            Spacer(
+                modifier = Modifier
+                    .fillMaxHeight(0.5f)
+                    .weight(1f)
+            )
+        }
+    }
 
     @Composable
-    fun TextRow (
+    private fun FinancesCard(
+        modifier: Modifier,
+        financesMain: FinancesMain
+    ) {
+        Card(
+            modifier = modifier
+                .fillMaxWidth(0.8f)
+                .clickable {
+
+                },
+            shape = RoundedCornerShape(16.dp),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 10.dp
+            )
+        ) {
+            Box(
+                modifier = modifier
+                    .fillMaxSize()
+                    .background(colorResource(R.color.md_theme_dark_primaryContainer))
+            ) {
+                Column(modifier = modifier) {
+                    TextRow("Amount Left", financesMain.amount_left)
+                    TextRow("Bank Amount", financesMain.bank)
+                    TextRow("Cash Amount", financesMain.cash)
+                    TextRow("Expenses", financesMain.expenses)
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun WorkoutHome(modifier: Modifier = Modifier) {
+        var workout by remember {
+            mutableStateOf(Workout(0, "", emptyList(), "0,1,2,3,4,5,6"))
+        }
+        LaunchedEffect(key1 = true) {
+            workout = workoutService.getWorkouts()[0]
+        }
+        Column(
+            modifier = modifier
+        ) {
+            Text(
+                "Today's Workout:",
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .padding(start = 40.dp, top = 22.dp)
+                    .weight(2f),
+                fontSize = 28.sp,
+                color = colorResource(R.color.md_theme_dark_onBackground),
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .weight(3f)
+            ) {
+                WorkoutCard(
+                    workout, Modifier
+                        .fillMaxWidth(0.85f)
+                        .align(Alignment.Center)
+                )
+            }
+            Spacer(
+                modifier = Modifier
+                    .width(100.dp)
+                    .weight(2f)
+            )
+        }
+    }
+
+    @Composable
+    fun WorkoutCard(
+        workout: Workout,
+        modifier: Modifier = Modifier
+    ) {
+        Card(
+            modifier = modifier
+                .fillMaxSize()
+                .clickable {
+
+                },
+            shape = RoundedCornerShape(16.dp),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 10.dp
+            )
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(colorResource(R.color.md_theme_dark_primaryContainer))
+            ) {
+                Column(
+                    modifier = modifier,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text(
+                        text = workout.name,
+                        fontSize = 24.sp,
+                        color = colorResource(R.color.md_theme_dark_onPrimaryContainer),
+                        modifier = Modifier.padding(bottom = 10.dp, top = 10.dp)
+                    )
+                    for (exercise: Exercise in workout.exercises) {
+                        Text(
+                            text = exercise.name,
+                            fontSize = 22.sp,
+                            color = colorResource(R.color.md_theme_dark_onPrimaryContainer)
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun TextRow(
         text: String,
         sum: Int,
         modifier: Modifier = Modifier
     ) {
-        Row (
+        Row(
             modifier = modifier
                 .fillMaxWidth()
                 .padding(16.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text (
+            Text(
                 text = text,
-                color = colorResource(R.color.md_theme_dark_onBackground),
+                color = colorResource(R.color.md_theme_dark_onPrimaryContainer),
                 fontSize = 28.sp
             )
-            Text (
+            Text(
                 text = sum.toString(),
-                color = colorResource(R.color.md_theme_dark_onBackground),
+                color = colorResource(R.color.md_theme_dark_onPrimaryContainer),
                 fontSize = 28.sp
             )
         }
-    }
-
-//    private fun update() {
-//        launch {
-//            val newData = financesService.getMainInfo()
-//            if (newData != null) {
-//                binding.tvAmountLeft.text = newData.amount_left.toString()
-//                binding.tvBank.text = newData.bank.toString()
-//                binding.tvCash.text = newData.cash.toString()
-//                binding.tvExpenses.text = newData.expenses.toString()
-//                binding.swipeRefresh.isRefreshing = false
-//            }
-//        }
-//    }
-
-    override fun onResume() {
-        super.onResume()
-//        update()
     }
 
     override fun onValueSelected(e: Entry?, h: Highlight?) {
         if (e == null)
             return
         if (h != null) {
-            Log.i("VAL SELECTED",
+            Log.i(
+                "VAL SELECTED",
                 "Value: " + e.y + ", index: " + h.x
                         + ", DataSet index: " + h.dataSetIndex
             )
