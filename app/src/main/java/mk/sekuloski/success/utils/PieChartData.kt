87d9899fd0f6f3d216b9b9e6d049b91b68388536
Sekuloski <sekuloski.bojan18@gmail.com>
2023-06-23 23:21:04 +0200

    feat: Category reset refactor

--- app/src/main/java/mk/sekuloski/success/utils/PieChartData.kt
+++ app/src/main/java/mk/sekuloski/success/utils/PieChartData.kt
@@ -130,10 +130,8 @@ fun setData(
 }
 
 fun resetCategories(): ArrayList<Int> {
-    val categories = ArrayList<Int>()
-    for (category: ExpenseType in ExpenseType.values())
-    {
-        categories.add(0)
-    }
+    val categories = ArrayList<Int>(ExpenseType.values().size)
+    categories.fill(0)
+
     return categories
 }
