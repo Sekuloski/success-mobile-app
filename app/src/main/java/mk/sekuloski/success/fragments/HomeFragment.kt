de2360429fb5abf90207c1ca7660a4287c4fb62e
Sekuloski <sekuloski.bojan18@gmail.com>
2023-06-25 15:40:52 +0200

    feat: Design improvements

--- app/src/main/java/mk/sekuloski/success/fragments/HomeFragment.kt
+++ app/src/main/java/mk/sekuloski/success/fragments/HomeFragment.kt
@@ -16,8 +16,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -25,8 +25,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
@@ -75,6 +79,7 @@ class HomeFragment(
         Column(
             modifier = modifier
         ) {
+            Greeting()
             FinancesHome(modifier.weight(1f))
             WorkoutHome(modifier.weight(1f))
         }
@@ -92,7 +97,7 @@ class HomeFragment(
             modifier = modifier
         ) {
             Text(
-                "Financial Situation:",
+                "Your Financial Situation:",
                 modifier = modifier
                     .weight(1f)
                     .fillMaxSize()
@@ -118,7 +123,7 @@ class HomeFragment(
             Spacer(
                 modifier = Modifier
                     .fillMaxHeight(0.5f)
-                    .weight(1f)
+                    .weight(0.5f)
             )
         }
     }
@@ -130,10 +135,7 @@ class HomeFragment(
     ) {
         Card(
             modifier = Modifier
-                .fillMaxWidth(0.8f)
-                .clickable {
-
-                },
+                .fillMaxWidth(0.8f),
             shape = RoundedCornerShape(16.dp),
             elevation = CardDefaults.cardElevation(
                 defaultElevation = 10.dp
@@ -143,6 +145,15 @@ class HomeFragment(
                 modifier = modifier
                     .fillMaxSize()
                     .background(MaterialTheme.colorScheme.primaryContainer)
+                    .clickable {
+                        requireActivity().supportFragmentManager
+                            .beginTransaction()
+                            .apply {
+                                replace(R.id.flFragment, FinancesFragment(financesService))
+                                addToBackStack(null)
+                                commit()
+                            }
+                    }
             ) {
                 Column(modifier = modifier) {
                     TextRow("Amount Left", financesMain.amount_left)
@@ -191,8 +202,7 @@ class HomeFragment(
             }
             Spacer(
                 modifier = Modifier
-                    .width(100.dp)
-                    .weight(2f)
+                    .weight(1f)
             )
         }
     }
@@ -204,10 +214,7 @@ class HomeFragment(
     ) {
         Card(
             modifier = Modifier
-                .fillMaxWidth(0.8f)
-                .clickable {
-
-                },
+                .fillMaxWidth(0.8f),
             shape = RoundedCornerShape(16.dp),
             elevation = CardDefaults.cardElevation(
                 defaultElevation = 10.dp
@@ -217,24 +224,43 @@ class HomeFragment(
                 modifier = modifier
                     .fillMaxSize()
                     .background(MaterialTheme.colorScheme.tertiaryContainer)
+                    .clickable {
+                        requireActivity().supportFragmentManager
+                            .beginTransaction()
+                            .apply {
+                                replace(R.id.flFragment, WorkoutFragment(workout))
+                                addToBackStack(null)
+                                commit()
+                            }
+                    }
             ) {
                 Column(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier
+                        .fillMaxSize()
                         .padding(16.dp),
                     horizontalAlignment = Alignment.Start
                 ) {
-                    Text(
-                        text = workout.name,
-                        fontSize = 24.sp,
-                        color = MaterialTheme.colorScheme.onTertiaryContainer,
-                        modifier = Modifier.padding(bottom = 10.dp, top = 10.dp)
-                    )
-                    for (exercise: Exercise in workout.exercises) {
+                    Box (
+                        modifier = Modifier.padding(bottom = 10.dp, top = 10.dp).align(CenterHorizontally)
+                            ){
+                        val fontSize = 24.sp
+                        ShadowText(fontSize, workout.name)
                         Text(
-                            text = exercise.name,
-                            fontSize = 22.sp,
-                            color = MaterialTheme.colorScheme.onTertiaryContainer
+                            text = workout.name,
+                            fontSize = fontSize,
+                            color = MaterialTheme.colorScheme.onTertiaryContainer,
                         )
+                    }
+                    for (exercise: Exercise in workout.exercises) {
+                        val fontSize = 22.sp
+                        Box {
+                            ShadowText(fontSize, exercise.name)
+                            Text(
+                                text = exercise.name,
+                                fontSize = fontSize,
+                                color = MaterialTheme.colorScheme.onTertiaryContainer
+                            )
+                        }
                     }
                 }
             }
@@ -254,16 +280,61 @@ class HomeFragment(
                 .padding(16.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text(
-                text = text,
-                color = MaterialTheme.colorScheme.onPrimaryContainer,
-                fontSize = 28.sp
-            )
-            Text(
-                text = amount.toString(),
-                color = MaterialTheme.colorScheme.onPrimaryContainer,
-                fontSize = 28.sp
-            )
+            val fontSize = 28.sp
+            Box {
+                ShadowText(fontSize, text)
+                Text(
+                    text = text,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    fontSize = fontSize
+                )
+            }
+            Box {
+                ShadowText(fontSize, amount.toString())
+                Text(
+                    text = amount.toString(),
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    fontSize = fontSize
+                )
+            }
+        }
+    }
+
+    @Composable
+    private fun ShadowText(
+        fontSize: TextUnit,
+        text: String
+    ) {
+        Text(
+            modifier = Modifier
+                .alpha(alpha = 0.5f)
+                .offset(x = 2.dp, y = 1.dp)
+                .blur(radius = 1.dp),
+            color = Color.Black,
+            fontSize = fontSize.times(1.03f),
+            text = text
+        )
+    }
+
+    @Composable
+    fun Greeting() {
+        Text(
+            text = "Good ${getTimeOfDay()}!",
+            fontSize = 45.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            modifier = Modifier
+                .padding(16.dp)
+                .padding(start = 48.dp, top = 36.dp),
+        )
+    }
+
+    private fun getTimeOfDay(): String {
+        val currentTime = LocalTime.now()
+        return when (currentTime.hour) {
+            in 0..11 -> "Morning"
+            in 12..16 -> "Afternoon"
+            in 17..19 -> "Evening"
+            else -> "Night"
         }
     }
 
