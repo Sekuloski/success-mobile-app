f3c4fa611d8f24765069f3666283f68c43ab253c
Sekuloski <sekuloski.bojan18@gmail.com>
2023-07-06 15:58:51 +0200

    feat: MainActivity migrated to Jetpack Compose

--- app/src/main/java/mk/sekuloski/success/MainActivity.kt
+++ app/src/main/java/mk/sekuloski/success/MainActivity.kt
@@ -2,47 +2,61 @@ package mk.sekuloski.success
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.NavGraph
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import mk.sekuloski.success.data.remote.dto.finances.FinancesMain
+import mk.sekuloski.success.data.remote.dto.workouts.Exercise
+import mk.sekuloski.success.data.remote.dto.workouts.Workout
 import mk.sekuloski.success.data.remote.services.finances.FinancesService
 import mk.sekuloski.success.data.remote.services.workouts.WorkoutsService
-import mk.sekuloski.success.databinding.ActivityMainBinding
-import mk.sekuloski.success.fragments.finances.FinancesFragment
-import mk.sekuloski.success.fragments.HomeFragment
-import mk.sekuloski.success.fragments.workouts.WorkoutsFragment
+import mk.sekuloski.success.fragments.NavGraphs
+import mk.sekuloski.success.ui.theme.AppTheme
+import mk.sekuloski.success.utils.normalizeWorkouts
+import java.time.LocalTime
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private val financesService = FinancesService.create()
-    private val workoutService = WorkoutsService.create()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
-        val homeFragment = HomeFragment(financesService, workoutService)
-        setCurrentFragment(homeFragment)
-        binding.bottomNavigationView.selectedItemId = R.id.miHome
-
-        val financesFragment = FinancesFragment(financesService)
-        val workoutsFragment = WorkoutsFragment(workoutService)
-
-        binding.bottomNavigationView.setOnItemSelectedListener{
-            when(it.itemId)   {
-                R.id.miFinances -> setCurrentFragment(financesFragment)
-                R.id.miWorkouts -> setCurrentFragment(workoutsFragment)
-                else -> setCurrentFragment(homeFragment)
+        setContent {
+            AppTheme {
+                DestinationsNavHost(navGraph = NavGraphs.root)
             }
-            true
         }
     }
-
-    private fun setCurrentFragment(fragment: Fragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment, fragment)
-            addToBackStack(null)
-            commit()
-        }
 
 }
